889ba0a85e54434de942c9c2da080d09d294003e
rmdk <ryan@rmdk.ca>
2014-11-15 13:45:01 -0500

    added user sessions

--- db_create.py
+++ db_create.py
@@ -9,7 +9,7 @@ db.create_all()
 
 # Insert some dummy data
 
-db.session.add(Task('Finish this app', date(2014, 12, 30), 10, 1))
-db.session.add(Task('Finish v1.0 of dashboard', date(2014, 12, 10), 10, 1))
+# db.session.add(Task('Finish this app', date(2014, 12, 30), 10, 1))
+# db.session.add(Task('Finish v1.0 of dashboard', date(2014, 12, 10), 10, 1))
 
-db.session.commit()
+# db.session.commit()
--- forms.py
+++ forms.py
@@ -1,17 +1,34 @@
 # forms.py
 
 from flask_wtf import Form
-from wtforms import TextField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import TextField, DateField, IntegerField, SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo, Required
+from wtforms import validators
 
 class AddTaskForm(Form):
 	task_id = IntegerField('Priority')
 	name = TextField('Task Name', validators=[DataRequired()])
 	due_date = DateField(
-		'Date Due (mm/dd/yyyy',
-			validators=[DataRequired()], format='%m/%d/%Y')
+		'Date Due (mm-dd-yyyy)',
+			validators=[DataRequired()], format='%m-%d-%Y')
 	priority = SelectField(
 		'Priority',
-		 choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),
-				('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
+		choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'),('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
 	status = IntegerField('Status')
+
+class RegisterForm(Form):
+    name = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+
+class LoginForm(Form):
+	name = TextField('Usernames', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+
+
+
